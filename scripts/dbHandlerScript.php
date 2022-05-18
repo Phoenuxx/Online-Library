d9c6fd17e364c0c71cb30a87a76def49ff6420f5
Phoenuxx <bdstevenson4225@aol.com>
2022-05-18 00:23:53 -0400

    attempting push to heroku

--- scripts/dbHandlerScript.php
+++ scripts/dbHandlerScript.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "library";
+$dbServerName = "ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbUsername = "qnhbmhejmme5tt1t";
+$dbPassword = "ypndcftdfh8ib8yt6";
+$dbName = "fi3kex7r9j7h9bzs";
 
 $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName );
 
